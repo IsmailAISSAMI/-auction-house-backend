33c2637e0ede63d63819717013ed658c71414fe7
ismailaissami <aissami.ismail@gmail.com>
2021-05-04 18:45:01 +0200

    update product

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -52,3 +52,44 @@ exports.getProducts = async (req, res, next) => {
         });
     }
 }
+
+exports.getProduct = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const product = await firestore.collection('products').doc(id);
+        const data = await product.get();
+        if(!data.exists) {
+            res.status(404).send({
+                error: 404,
+                message: err.message || 'Product with the given ID not found! try again.',
+            });
+        }else {
+            res.send(data.data());
+        }
+    } catch (error) {
+        res.status(400).send({
+            error: 400,
+            message: err.message || 'Some error occured while getting the product!',
+        });
+    }
+}
+
+exports.update = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const data = req.body;
+        const product =  await firestore.collection('products').doc(id);
+        await product.update(data);
+        const newdata = await product.get();
+
+        res.send({
+            message: 'Product record was updated successfuly!',
+            product: newdata.data()
+        });        
+    } catch (error) {
+        res.status(400).send({
+            error: 400,
+            message: err.message || 'Some error occured while updating the product!',
+        });
+    }
+}
--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -4,11 +4,11 @@ const product = require('../controllers/products.controller');
 
 //ROUTES FOR ADMIN
 router.post('/products' ,product.create);
-// router.patch('/products/:id', product.update);
+router.patch('/products/:id', product.update);
 // router.delete('/products/:id', product.delete);
 
 // ROUTES FOR USER LAMBDA
 router.get('/products/', product.getProducts);
-// router.get('/products/:id', product.getProduct);
+router.get('/products/:id', product.getProduct);
 
 module.exports = router;
